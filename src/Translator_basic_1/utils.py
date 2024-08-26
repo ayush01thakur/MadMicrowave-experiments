ed73baef5a0055b055706f3b159358fe5524e869
ayush01thakur <thakur8ayush@gmail.com>
2024-08-26 22:45:52 +0530

    modified translate app prompt

--- src/Translator_basic_1/utils.py
+++ src/Translator_basic_1/utils.py
@@ -8,13 +8,24 @@ def Translation70b(sourceLanguage, targetLanguage, tone, inputText):
     chat_completion = groq_client.chat.completions.create(
     messages=[
             {
+                # "role": "system",
+                # "content": """You are a highly proficient translator. 
+                # Your task is to faithfully translate the given input text from input language to given output language. while preserving the meaning. 
+                # Remeber the given tone and actual meaning into consideration (just translate and add the little tone to the translated text and don't change the meaning in the output language and give output only in output language provided). 
+                # Recognize the idioms and converstional gestures/tone and then give the natural translations. 
+                # Remember to only output the translated text in the output language (provided) and nothing more. Do not give any advice/review based on input text, just give the translation.
+                # (If input Text is an url leave the url as it is)."""
+                
                 "role": "system",
-                "content": """You are a highly proficient translator. 
-                Your task is to faithfully translate the given input text from input language to given output language while preserving the meaning. 
-                Remeber the given tone and actual meaning into consideration (just translate and add the little tone to the translated text and don't change the meaning in the output language and give output only in output language provided). 
-                Recognize the idioms and converstional gestures/tone and then give the natural translations. 
-                Remember to only output the translated text in the output language (provided) and nothing more. 
-                (If input Text is an url leave the url as it is)."""
+                "content": """You are a highly proficient translator. Your task is to:
+
+                1. Faithfully translate the given input text from the input language to the output language in given tone (tone can be like Normal, sarcastic, exaggeration, angry, etc).
+                2. Recognize and naturally translate idioms and conversational gestures/tone.
+                3. Only output the translated text in the specified output language (provided) in specified tone.
+                4. Do not provide any additional commentary, advice, or review based on the input text.
+                5. Leave URLs untranslated.
+
+                Provide only the translation in the output language, nothing more."""
 
             },
             {
