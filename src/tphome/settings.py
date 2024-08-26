27881f35304bf37ac6a85fa91d6aa004c133545b
ayush01thakur <thakur8ayush@gmail.com>
2024-08-26 07:06:18 +0530

    .

--- src/tphome/settings.py
+++ src/tphome/settings.py
@@ -153,5 +153,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
